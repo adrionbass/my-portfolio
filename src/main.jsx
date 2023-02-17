4353ee10392940fbd4c2038421e21c681079a921
Adriel Miño <adrielsmb@gmail.com>
2023-02-17 11:41:38 -0300

    17/2/2023k

--- src/main.jsx
+++ src/main.jsx
@@ -38,8 +38,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
     <Scaffold id="more-info" title="MAS SOBRE MI">
 
-    Asi es, dije que considero a la programación como un arte, y puedo demostrarlo.
-    Pero en principio quiero mostrar algo sobre mi vínculo con la música...Aquí van imágenes (...)
+
+     <p>Asi es, dije que considero a la programación como un arte, y puedo demostrarlo. Agregar imágenes.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
     </Scaffold>
 
     <Scaffold id="ctt" title="CONTACTO">
